61278ca806fdb49e01087686e873b8013726ece6
Vishnu Vardhan Reddy <112470858+vishnu-912@users.noreply.github.com>
2023-06-05 21:40:51 +0530

    perform 1st DOM manipulation

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Being Zero")
+var data1={image: "IMAGE.jpg",name : "Vishnu Vardhan Reddy",designation:"Software Engineer"};
+var data2={image: "IMAGE2.jpg",name : "VISHNU",designation:"Software Developer"};
+var isdata1= true;
+var displaydata;
+var changemypic = function(){
+    if (isdata1){
+        displaydata=data2;
+        isdata1 = false;
+    }
+    else{
+        displaydata = data1;
+        isdata1=false;
+    }
+    document.getElementById("myimage").src=displaydata.image;
+}
